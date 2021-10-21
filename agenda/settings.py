7f0400632c10a387442f919ed2017f6d14d5683a
unknown <gabriel.silvam@outlook.com.br>
2021-10-21 11:49:23 -0300

    Up_Agenda

--- agenda/settings.py
+++ agenda/settings.py
@@ -18,9 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uwkka!!^_93(fux#jhph)8*)4q9c+e0khwi=_k9np@^#@*hb-z'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
